a2eb9e08f54d65376a28624520cfb18649924989
aryansenpaii <aryansenpai256@gmail.com>
2024-11-17 16:32:14 +0530

    project-pre-build

---
+++ src/constants/categoryData.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const categoryData = () => {
+  return (
+    <View>
+      <Text>categoryData</Text>
+    </View>
+  )
+}
+
+export default categoryData
